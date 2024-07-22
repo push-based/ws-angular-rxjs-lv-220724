5e2e1837c5950a90f243a6d8e0179aa6fa0d94c7
Julian Jandl <julian@jandl.wien>
2024-07-22 14:27:36 +0200

    feat: implement solution-7: exhaustMap

--- src/app/movie/movie-list-page/movie-list-page.component.ts
+++ src/app/movie/movie-list-page/movie-list-page.component.ts
@@ -2,9 +2,17 @@ import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FastSvgComponent } from '@push-based/ngx-fast-svg';
-import { startWith, switchMap } from 'rxjs';
+import {
+  exhaustMap,
+  Observable,
+  scan,
+  startWith,
+  Subject,
+  switchMap,
+} from 'rxjs';
 
 import { ElementVisibilityDirective } from '../../shared/cdk/element-visibility/element-visibility.directive';
+import { TMDBMovieModel } from '../../shared/model/movie.model';
 import { MovieService } from '../movie.service';
 import { MovieListComponent } from '../movie-list/movie-list.component';
 
@@ -13,7 +21,7 @@ import { MovieListComponent } from '../movie-list/movie-list.component';
   template: `
     @if (movies$ | async; as movies) {
       <movie-list [movies]="movies" />
-      <!-- <div (elementVisible)=""></div> -->
+      <div (elementVisible)="paginate$.next()"></div>
     } @else {
       <div class="loader"></div>
     }
@@ -30,17 +38,31 @@ export class MovieListPageComponent {
   private activatedRoute = inject(ActivatedRoute);
   private movieService = inject(MovieService);
 
+  paginate$ = new Subject<void>();
+
   movies$ = this.activatedRoute.params.pipe(
     switchMap((params) => {
       if (params.category) {
-        return this.movieService
-          .getMovieList(params.category)
-          .pipe(startWith(undefined));
+        return this.paginate((page) => {
+          return this.movieService.getMovieList(params.category, page);
+        });
       } else {
-        return this.movieService
-          .getMoviesByGenre(params.id)
-          .pipe(startWith(undefined));
+        return this.paginate((page) => {
+          return this.movieService.getMoviesByGenre(params.id, page);
+        });
       }
     }),
   );
+
+  private paginate(paginateFn: (page: number) => Observable<TMDBMovieModel[]>) {
+    return this.paginate$.pipe(
+      startWith(void 0),
+      exhaustMap((_, i) => {
+        return paginateFn(i + 1);
+      }),
+      scan((allMovies, pagedMovies) => {
+        return [...allMovies, ...pagedMovies];
+      }, [] as TMDBMovieModel[]),
+    );
+  }
 }
