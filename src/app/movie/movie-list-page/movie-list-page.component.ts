14373fad9efabc7562cc52a9ed2c9ca3431e8052
Julian Jandl <julian@jandl.wien>
2024-07-22 15:30:08 +0200

    feat: implement solution-8: mergeMap

--- src/app/movie/movie-list-page/movie-list-page.component.ts
+++ src/app/movie/movie-list-page/movie-list-page.component.ts
@@ -3,7 +3,10 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FastSvgComponent } from '@push-based/ngx-fast-svg';
 import {
+  BehaviorSubject,
   exhaustMap,
+  map,
+  mergeMap,
   Observable,
   scan,
   startWith,
@@ -20,7 +23,12 @@ import { MovieListComponent } from '../movie-list/movie-list.component';
   selector: 'movie-list-page',
   template: `
     @if (movies$ | async; as movies) {
-      <movie-list [movies]="movies" />
+      <movie-list
+        [favoriteMovieIds]="(favoriteIds$ | async)!"
+        [favoritesLoading]="(favoritesLoading$ | async)!"
+        (favoriteToggled)="toggleFavorite$.next($event)"
+        [movies]="movies"
+      />
       <div (elementVisible)="paginate$.next()"></div>
     } @else {
       <div class="loader"></div>
@@ -38,21 +46,65 @@ export class MovieListPageComponent {
   private activatedRoute = inject(ActivatedRoute);
   private movieService = inject(MovieService);
 
+  // trigger
+  toggleFavorite$ = new Subject<TMDBMovieModel>();
   paginate$ = new Subject<void>();
+
+  // state
+  favoriteIds$ = new BehaviorSubject(new Set<string>());
+  favoritesLoading$ = new BehaviorSubject(new Set<string>());
 
   movies$ = this.activatedRoute.params.pipe(
     switchMap((params) => {
       if (params.category) {
         return this.paginate((page) => {
           return this.movieService.getMovieList(params.category, page);
-        });
+        }).pipe(startWith(undefined));
       } else {
         return this.paginate((page) => {
           return this.movieService.getMoviesByGenre(params.id, page);
-        });
+        }).pipe(startWith(undefined));
       }
     }),
   );
+
+  constructor() {
+    this.toggleFavorite$
+      .pipe(
+        mergeMap((movie) =>
+          this.movieService.toggleFavorite(movie).pipe(
+            map((isFavorite) => {
+              const favoriteIds = this.favoriteIds$.getValue();
+              if (isFavorite) {
+                favoriteIds.add(movie.id);
+              } else {
+                favoriteIds.delete(movie.id);
+              }
+              const favoritesLoading = this.favoritesLoading$.getValue();
+              favoritesLoading.delete(movie.id);
+              return {
+                favoriteIds: new Set(favoriteIds),
+                favoritesLoading: new Set(favoritesLoading),
+              };
+            }),
+            startWith<{
+              favoritesLoading: Set<string>;
+              favoriteIds?: Set<string>;
+            }>({
+              favoritesLoading: new Set(
+                this.favoritesLoading$.getValue().add(movie.id),
+              ),
+            }),
+          ),
+        ),
+      )
+      .subscribe(({ favoriteIds, favoritesLoading }) => {
+        if (favoriteIds) {
+          this.favoriteIds$.next(favoriteIds);
+        }
+        this.favoritesLoading$.next(favoritesLoading);
+      });
+  }
 
   private paginate(paginateFn: (page: number) => Observable<TMDBMovieModel[]>) {
     return this.paginate$.pipe(
