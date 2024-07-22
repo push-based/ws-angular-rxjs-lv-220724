3131d2aa4d80dfa6920047a1c98b90001993200b
Julian Jandl <julian@jandl.wien>
2024-07-22 13:49:33 +0200

    feat: implement solution-6: switchMap

--- src/app/movie/movie-list-page/movie-list-page.component.ts
+++ src/app/movie/movie-list-page/movie-list-page.component.ts
@@ -1,38 +1,46 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FastSvgComponent } from '@push-based/ngx-fast-svg';
+import { startWith, switchMap } from 'rxjs';
 
 import { ElementVisibilityDirective } from '../../shared/cdk/element-visibility/element-visibility.directive';
-import { TMDBMovieModel } from '../../shared/model/movie.model';
 import { MovieService } from '../movie.service';
 import { MovieListComponent } from '../movie-list/movie-list.component';
 
 @Component({
   selector: 'movie-list-page',
   template: `
-    <movie-list [movies]="movies" />
-    <!-- <div (elementVisible)=""></div> -->
+    @if (movies$ | async; as movies) {
+      <movie-list [movies]="movies" />
+      <!-- <div (elementVisible)=""></div> -->
+    } @else {
+      <div class="loader"></div>
+    }
   `,
   standalone: true,
-  imports: [MovieListComponent, ElementVisibilityDirective, FastSvgComponent],
+  imports: [
+    MovieListComponent,
+    ElementVisibilityDirective,
+    FastSvgComponent,
+    AsyncPipe,
+  ],
 })
 export class MovieListPageComponent {
   private activatedRoute = inject(ActivatedRoute);
   private movieService = inject(MovieService);
 
-  movies: TMDBMovieModel[] = [];
-
-  constructor() {
-    this.activatedRoute.params.subscribe((params) => {
+  movies$ = this.activatedRoute.params.pipe(
+    switchMap((params) => {
       if (params.category) {
-        this.movieService.getMovieList(params.category).subscribe((movies) => {
-          this.movies = movies;
-        });
+        return this.movieService
+          .getMovieList(params.category)
+          .pipe(startWith(undefined));
       } else {
-        this.movieService.getMoviesByGenre(params.id).subscribe((movies) => {
-          this.movies = movies;
-        });
+        return this.movieService
+          .getMoviesByGenre(params.id)
+          .pipe(startWith(undefined));
       }
-    });
-  }
+    }),
+  );
 }
